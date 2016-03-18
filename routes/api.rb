70ee698856619adba347202de68075a0a8cf3dcf
Gerrit Riessen <gerrit.riessen@gmail.com>
2016-03-18 12:04:53 +0100

    damn, first_or_create should be find_or_create_by

--- routes/api.rb
+++ routes/api.rb
@@ -1,7 +1,7 @@
 post '/api/:version/create' do
   cl = JSON.parse(params[:campaign_link])
   CampaignLink.
-    first_or_create(:id => cl["id"]).
+    find_or_create_by(:id => cl["id"]).
     update(cl)
   $refresh_cam_lnk_cache.call
   json({ :status => :ok })
